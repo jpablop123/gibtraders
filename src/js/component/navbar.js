74fd2e48e5fd83c9ffc04c9e3da44aa9378b2ebe
jpablop123 <juanpablopinto123@icloud.com>
2023-06-08 23:38:19 +0000

    avance

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,17 +1,133 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../../styles/navbar.css";
 
-export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
-			</div>
-		</nav>
-	);
+
+
+const Navbar = () => {
+  const menuToggle = () => {
+    let menuHolder = document.getElementById('menuHolder')
+    let siteBrand = document.getElementById('siteBrand')
+    function menuToggle(){
+      if(menuHolder.className === "drawMenu") menuHolder.className = ""
+      else menuHolder.className = "drawMenu"
+    }
+    if(window.innerWidth < 426) siteBrand.innerHTML = "MAS"
+    window.onresize = function(){
+      if(window.innerWidth < 420) siteBrand.innerHTML = "MAS"
+      else siteBrand.innerHTML = "MY AWESOME WEBSITE"
+    }
+  };
+
+  return (
+    <nav>
+      <div className="superNav border-bottom py-2 bg-light">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12 centerOnMobile">
+              <select className="me-3 border-0 bg-light">
+                <option value="en-us">EN-US</option>
+              </select>
+              <span className="d-none d-lg-inline-block d-md-inline-block d-sm-inline-block d-xs-none me-3">
+                <strong>felipepint@gibtraders.com</strong>
+              </span>
+              <span className="me-3">
+                <i className="fa-solid fa-phone me-1 text-warning"></i>{' '}
+                <strong>+1-786-474-2836</strong>
+              </span>
+            </div>
+            <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12 d-none d-lg-block d-md-block-d-sm-block d-xs-none text-end">
+              <span className="me-3">
+                <i className="fa-solid fa-truck text-muted me-1"></i>
+                <a className="text-muted" href="#">
+                  Shipping
+                </a>
+              </span>
+              <span className="me-3">
+                <i className="fa-solid fa-file text-muted me-2"></i>
+                <a className="text-muted" href="#">
+                  Policy
+                </a>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <nav className="navbar navbar-expand-lg bg-white sticky-top navbar-light p-3 shadow-sm">
+        <div className="container">
+          <a className="navbar-brand" href="#">
+            <i className="fa-solid fa-shop me-2"></i> <strong>Gib Traders Corp</strong>
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavDropdown"
+            aria-controls="navbarNavDropdown"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="mx-auto my-3 d-lg-none d-sm-block d-xs-block">
+            <div className="input-group">
+              <span className="border-warning input-group-text bg-warning text-white">
+                <i className="fa-solid fa-magnifying-glass"></i>
+              </span>
+              <input
+                type="text"
+                className="form-control border-warning"
+                style={{ color: '#7a7a7a' }}
+              />
+              <button className="btn btn-warning text-white">search</button>
+            </div>
+          </div>
+          <div className="collapse navbar-collapse" id="navbarNavDropdown">
+            <div className="ms-auto d-none d-lg-block">
+           
+            </div>
+            <ul className="navbar-nav ms-auto ">
+              <li className="nav-item">
+                <a
+                  className="nav-link mx-2 text-uppercase active"
+                  aria-current="page"
+                  href="#"
+                >
+                  hola
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link mx-2 text-uppercase" href="#">
+                  Ubicacion
+                </a>
+              </li>
+              
+              <li className="nav-item">
+                <a className="nav-link mx-2 text-uppercase" href="#">
+                  Servicios
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link mx-2 text-uppercase" href="#">
+                  Acerca de Gib Traders
+                </a>
+              </li>
+            </ul>
+            <ul className="navbar-nav ms-auto ">
+              <li className="nav-item">
+             
+              </li>
+              <li className="nav-item">
+                <a className="nav-link mx-2 text-uppercase" href="#">
+                  <i className="fa-solid fa-circle-user me-1"></i> Registrate
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </nav>
+  );
 };
+export default Navbar;
