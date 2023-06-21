01920371fa0aa904f63b60f06538e90218a4ffe0
jpablop123 <juanpablopinto123@icloud.com>
2023-06-21 19:02:54 +0000

    avance

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -2,21 +2,19 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "../../styles/navbar.css";
 
-
-
 const Navbar = () => {
   const menuToggle = () => {
-    let menuHolder = document.getElementById('menuHolder')
-    let siteBrand = document.getElementById('siteBrand')
-    function menuToggle(){
-      if(menuHolder.className === "drawMenu") menuHolder.className = ""
-      else menuHolder.className = "drawMenu"
+    let menuHolder = document.getElementById("menuHolder");
+    let siteBrand = document.getElementById("siteBrand");
+    function menuToggle() {
+      if (menuHolder.className === "drawMenu") menuHolder.className = "";
+      else menuHolder.className = "drawMenu";
     }
-    if(window.innerWidth < 426) siteBrand.innerHTML = "MAS"
-    window.onresize = function(){
-      if(window.innerWidth < 420) siteBrand.innerHTML = "MAS"
-      else siteBrand.innerHTML = "MY AWESOME WEBSITE"
-    }
+    if (window.innerWidth < 426) siteBrand.innerHTML = "MAS";
+    window.onresize = function () {
+      if (window.innerWidth < 420) siteBrand.innerHTML = "MAS";
+      else siteBrand.innerHTML = "MY AWESOME WEBSITE";
+    };
   };
 
   return (
@@ -32,20 +30,26 @@ const Navbar = () => {
                 <strong>felipepint@gibtraders.com</strong>
               </span>
               <span className="me-3">
-                <i className="fa-solid fa-phone me-1 .text-secondary"></i>{' '}
+                <i className="fa-solid fa-phone me-1 .text-secondary"></i>{" "}
                 <strong>+1-786-474-2836</strong>
               </span>
             </div>
             <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12 d-none d-lg-block d-md-block-d-sm-block d-xs-none text-end">
               <span className="me-3">
                 <i className="fa-solid fa-truck text-muted me-1"></i>
-                <a className="text-muted" href="https://globuscargo.com/registrate/">
+                <a
+                  className="text-muted"
+                  href="https://globuscargo.com/registrate/"
+                >
                   Registrate
                 </a>
               </span>
               <span className="me-3">
                 <i className="fa-solid fa-file text-muted me-2"></i>
-                <a className="text-muted" href="https://globuscargo.com/ingreso-a-casillero/">
+                <a
+                  className="text-muted"
+                  href="https://globuscargo.com/ingreso-a-casillero/"
+                >
                   Casillero
                 </a>
               </span>
@@ -55,8 +59,14 @@ const Navbar = () => {
       </div>
       <nav className="navbar navbar-expand-lg bg-white sticky-top navbar-light p-3 shadow-sm">
         <div className="container">
-          <a className="navbar-brand" href="#"> <img className="imagengib" src="https://media.licdn.com/dms/image/D4E0BAQHuobQdZrgk8g/company-logo_200_200/0/1685540090610?e=1694649600&v=beta&t=xC1BxaHDWHH1P0b_7Gmf-GFjmnNzfbPEGTibEOvD9Rw"></img>
-            <i className=""></i> <strong className="strong">Gib Traders Corp</strong>
+          <a className="navbar-brand" href="#">
+            {" "}
+            <img
+              className="imagengib"
+              src="https://media.licdn.com/dms/image/D4E0BAQHuobQdZrgk8g/company-logo_200_200/0/1685540090610?e=1694649600&v=beta&t=xC1BxaHDWHH1P0b_7Gmf-GFjmnNzfbPEGTibEOvD9Rw"
+            ></img>
+            <i className=""></i>{" "}
+            <strong className="strong">Gib Traders Corp</strong>
           </a>
           <button
             className="navbar-toggler"
@@ -75,14 +85,10 @@ const Navbar = () => {
               <span className="border-warning input-group-text bg-warning text-white">
                 <i className="fa-solid fa-magnifying-glass"></i>
               </span>
-             
-              
             </div>
           </div>
           <div className="collapse navbar-collapse" id="navbarNavDropdown">
-            <div className="ms-auto d-none d-lg-block">
-           
-            </div>
+            <div className="ms-auto d-none d-lg-block"></div>
             <ul className="navbar-nav ms-auto ">
               <li className="nav-item">
                 <a
@@ -98,7 +104,7 @@ const Navbar = () => {
                   Ubicacion
                 </a>
               </li>
-              
+
               <li className="nav-item">
                 <a className="nav-link mx-2 text-uppercase" href="#">
                   Servicios
@@ -111,16 +117,17 @@ const Navbar = () => {
               </li>
             </ul>
             <ul className="navbar-nav ms-auto ">
-              <li className="nav-item">
-             
-              </li>
+              <li className="nav-item"></li>
               <li className="nav-item">
                 <a className="nav-link mx-2 text-uppercase" href="#">
-                <a href="https://globuscargo.com/registrate/" target="_blank" rel="noopener noreferrer">
-  Registrate
-</a>
+                  <a
+                    href="https://globuscargo.com/registrate/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Registrate
+                  </a>
                 </a>
-               
               </li>
             </ul>
           </div>
